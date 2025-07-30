8a3d6912329d870bfe31f372dbebde62e1fe6316
Y-Jimin <jak2899@naver.com>
2025-07-30 15:21:31 +0900

    2차시 과제 완료 - 라우터, 컨트롤러 분리

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use('/users', userRouter);
-app.use('/product', productRouter);
+app.use('/users', userRouter); // user 엔드포인트 처리
+app.use('/product', productRouter); // product 엔드포인트 처리
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
